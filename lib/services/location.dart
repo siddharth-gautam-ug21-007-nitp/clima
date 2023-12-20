18b1833b383df3090a16f47eb3267edfd895097a
siddharth-gautam-ug21-007-nitp <siddharthg.ug21.cs@nitp.ac.in>
2023-12-20 13:48:09 +0530

    Initial commit

---
+++ lib/services/location.dart
@@ -0,0 +1,27 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double latitude;
+  late double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          // Handle denied permission
+          return;
+        }
+      }
+
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
